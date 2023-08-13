1cba41a6aefe6bae3e84850bc5817c0a4a6b2c01
bahioganns <bahioganns@gmail.com>
2023-08-13 21:39:27 +0400

    Implement new functions to controller
    Implemented function to execute executable files
    Implemented function to open not executable files
    Implemented function to add new file from interface
    Added correct doc strings

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
 import eel
 from back.servicedb import Servicedb, Dbfile
-from back.controller import get_files_json
+from back.controller import get_files_json, execute_file, open_file, add_new_file
 
 eel.init("front")
-
-dbf0 = Dbfile("C:\\Users\\bahio\\Downloads\\temp.txt")
-cont1 = Servicedb()
-cont1.add_to_db(dbf0)
-dbf1 = Dbfile("C:\\Users\\bahio\\Downloads\\temp1.txt")
-cont1.add_to_db(dbf1)
 
 print(get_files_json())
 
