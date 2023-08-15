da00abeb1fe36caf968caca8a45bd88828af9a31
bahioganns <bahioganns@gmail.com>
2023-08-15 19:11:29 +0400

    back/controller: recogn for all ext, window always on top
    Added recognition for all extensions
    Added error output for file openeing
    Made window alway open on top

--- back/controller.py
+++ back/controller.py
@@ -24,6 +24,28 @@ def get_ex_extentions_list():
     return [ext.lower() for ext in pathext]
 
 
+def open_ex_file(path):
+    """Execute process by given id, can return error"""
+    error = ""
+    try:
+        subprocess.Popen(path)
+    except Exception as e:
+        error = str(e)
+    finally:
+        return error
+
+
+def open_nonex_file(path):
+    """Open not executable file with default app, can return error"""
+    error = ""
+    try:
+        os.startfile(path)
+    except Exception as e:
+        error = str(e)
+    finally:
+        return error
+
+
 # can't use python functino to check if file is executable
 @eel.expose
 def open_file(id):
@@ -32,16 +54,19 @@ def open_file(id):
     path = serv.get_path_by_id(id)
     file_extension = os.path.splitext(path)[1]
     if file_extension in get_ex_extentions_list():
-        subprocess.Popen(path)
+        return open_ex_file(path)
     else:
-        os.startfile(path)
+        return open_nonex_file(path)
 
 
 def get_filepath_from_explorer():
     """Open a file dialog and retrieve the selected file path."""
     root = tk.Tk()
     root.withdraw()
+    root.attributes("-topmost", True)
     file_path = filedialog.askopenfilename()
+    root.attributes("-topmost", False)
+    root.destroy()
     return file_path
 
 
